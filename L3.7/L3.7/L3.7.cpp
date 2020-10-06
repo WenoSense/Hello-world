86c8f8cc6456a91ca7ee2516522fde0369be0f90
Соломатин Кирилл Александрович <185506@kip.college>
2020-10-06 15:27:08 +0300

    123

---
+++ L3.7/L3.7/L3.7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "Enter a: " << endl;
+	cin >> a;
+	cout << "Enter b: " << endl;
+	cin >> b;
+
+	__asm
+	{
+		mov eax, a
+		mov ebx, b
+		xchg eax, ebx
+		mov a, eax
+		mov b, ebx
+	}
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+}
---
+++ L3/L3/L3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout << "Enter a: " << endl;
+	cin >> a;
+	cout << "Enter b: " << endl;
+	cin >> b;
+
+	__asm
+	{
+		mov eax, a
+		add eax, b
+		mov c, eax
+
+		mov ebx, c
+		sub ebx, b
+		mov b, ebx
+
+		mov ecx, c
+		sub ecx, a
+		mov a, ecx
+	}
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+}
